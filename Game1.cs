79729e6ad84aba290cd2fc9313f65e0f9a2d98e2
Tom Lima <tom@tomlima.me>
2025-03-14 17:43:24 -0300

    First blood

---
+++ Game1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ShootingTarget;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Texture2D targetSprite;
+    private Texture2D crosshairsSprite;
+    private Texture2D backgroundSprite;
+    
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        _graphics.PreferredBackBufferWidth = 1080;
+        _graphics.PreferredBackBufferHeight = 608;
+        _graphics.ApplyChanges();
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        targetSprite = Content.Load<Texture2D>("target");
+        backgroundSprite = Content.Load<Texture2D>("background");
+        crosshairsSprite = Content.Load<Texture2D>("crosshairs");
+        
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(targetSprite, new Vector2(150, 300), Color.White);
+        _spriteBatch.Draw(backgroundSprite, new Vector2(0, 0), Color.White);
+
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
