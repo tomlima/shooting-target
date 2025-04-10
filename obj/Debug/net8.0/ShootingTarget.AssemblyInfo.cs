ea9ce22c5cfd95c84fee337b06f281efb970a455
Tom Lima <tom@tomlima.me>
2025-04-10 16:58:19 -0300

    Add gitignore file

--- obj/Debug/net8.0/ShootingTarget.AssemblyInfo.cs
+++ obj/Debug/net8.0/ShootingTarget.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShootingTarget")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79729e6ad84aba290cd2fc9313f65e0f9a2d98e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca96998c6d990896c81c1b6898572f591af53cd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShootingTarget")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShootingTarget")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
