ca96998c6d990896c81c1b6898572f591af53cd3
Tom Lima <tom@tomlima.me>
2025-04-10 16:29:28 -0300

    Add targeting

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -8,9 +9,17 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-    private Texture2D targetSprite;
-    private Texture2D crosshairsSprite;
-    private Texture2D backgroundSprite;
+    private Texture2D _targetSprite;
+    private Texture2D _crosshairsSprite;
+    private Texture2D _backgroundSprite;
+    private SpriteFont _font;
+    
+    private Vector2 _targetPosition = new Vector2(100, 300);
+    private const int _targetRadius = 45;
+
+    private MouseState _mouseState;
+    private bool _mouseReleased;
+    private int _score = 0;
     
     public Game1()
     {
@@ -30,10 +39,10 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-        targetSprite = Content.Load<Texture2D>("target");
-        backgroundSprite = Content.Load<Texture2D>("background");
-        crosshairsSprite = Content.Load<Texture2D>("crosshairs");
-        
+        _targetSprite = Content.Load<Texture2D>("target");
+        _backgroundSprite = Content.Load<Texture2D>("background");
+        _crosshairsSprite = Content.Load<Texture2D>("crosshairs");
+        _font = Content.Load<SpriteFont>("galleryFont");
     }
 
     protected override void Update(GameTime gameTime)
@@ -41,7 +50,23 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
+        _mouseState  = Mouse.GetState();
+         
+        if (_mouseState.LeftButton == ButtonState.Pressed && _mouseReleased)
+        {
+            float mouseTargetDistance = Vector2.Distance(_targetPosition, _mouseState.Position.ToVector2());
+            Console.WriteLine(mouseTargetDistance);
+            if (mouseTargetDistance < _targetRadius)
+            {
+                _score++;
+            }
+            _mouseReleased = false;
+        }
+
+        if (_mouseState.LeftButton == ButtonState.Released)
+        {
+            _mouseReleased = true;
+        }
 
         base.Update(gameTime);
     }
@@ -50,9 +75,9 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
         _spriteBatch.Begin();
-        _spriteBatch.Draw(targetSprite, new Vector2(150, 300), Color.White);
-        _spriteBatch.Draw(backgroundSprite, new Vector2(0, 0), Color.White);
-
+        _spriteBatch.Draw(_backgroundSprite, new Vector2(0, 0), Color.White);
+        _spriteBatch.DrawString(_font, _score.ToString(), new Vector2(10, 10), Color.White);
+        _spriteBatch.Draw(_targetSprite, new Vector2(_targetPosition.X - _targetRadius, _targetPosition.Y - _targetRadius ), Color.White);
         _spriteBatch.End();
 
         base.Draw(gameTime);
